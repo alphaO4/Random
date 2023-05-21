5e0d732780201aac0ec58c4597cccf87a7680cc3
alphaO4 <48568909+alphaO4@users.noreply.github.com>
2023-05-21 17:25:51 +0200

    Fake KeePass Spammer, finall update

--- KeePassMini-Spamer.py
+++ KeePassMini-Spamer.py
@@ -1,3 +1,46 @@
-#!/usr/bin/python3
-import requests
-import faker
+import socket
+from faker import Faker
+
+fake = Faker()
+
+
+def sendSpam():
+    # Analyse String
+    anna = "!AMSG|"
+    if fake.boolean():
+        anna += "WIFI"
+    else:
+        anna += "CELL"
+
+    anna += "|" + fake.country() + "|" + fake.language_code()
+    anna += "|" + fake.timezone() + "|" + fake.word()
+    anna += "|" + str(fake.random_number(digits=1)) + "." + str(fake.random_number(digits=1)) + "." + str(fake.random_number(digits=1))
+    anna += "|" + fake.uuid4()
+    anna += "|" + str(fake.random_number(digits=1)) + "." + str(fake.random_number(digits=1)) + "." + str(fake.random_number(digits=1))
+    anna += "|" + fake.text()
+    anna += "|" + fake.word()
+    anna += "|" + str(fake.random_number(digits=5))
+    anna += "|" + fake.date()
+    anna += "|" + fake.date()
+    anna += "|" + str(fake.random_number(digits=50))
+    anna += "|" + fake.ipv4()
+
+    # UDP-Server
+    address = "anna.unicomedv.de"
+    port = 10548
+
+    # Create a UDP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    try:
+        client_socket.sendto(anna.encode(), (address, port))
+        print("UDP send data success")
+    except socket.error as error:
+        print(f"UDP send data error: {error}")
+    finally:
+        client_socket.close()
+
+
+for i in range(int(input("How many spam messages do you want to send?: "))):
+    sendSpam()
+    i += 1
